3671dce23a88d8cb7a0622094205b253adb3a133
lchan <leochan007@163.com>
2019-03-15 07:59:30 +0800

    update k8s

--- frontend_v3/src/components/Login/Login.vue
+++ frontend_v3/src/components/Login/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Login">
     <div class="title">Elemental Battles - powered by EOSIO</div>
-    <div class="description">Please use the Scatter EOS Account to log into the game.</div>
+    <div class="description">Please use the Scatter EOS Account to log into the game. You should have a Scatter client or a Scatter plugin in your browser.</div>
     <div class="field form-error">
       <span v-if="showError" class="error">{{ error }}</span>
     </div>
